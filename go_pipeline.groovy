fee79dbb1970a3296c1795d4d95efcc4dd63f05a
e5471 <soledadnatalia.kuczala@igt.com>
2020-06-22 15:37:17 +0200

    pipeline builds docker-compose and dockerfile by input parameter choice

--- go_pipeline.groovy
+++ go_pipeline.groovy
@@ -1,37 +1,65 @@
 pipeline {
-  	options { timestamps () }
-  	agent {label 'Dockerlab-Agent'}
-  	tools {
+      options { timestamps () }
+      agent {label 'Dockerlab-Agent'}
+      tools {
         go 'GO'
+      }
+
+    parameters {
+        choice(name: 'MODE', choices: ['api', 'cli'], description: 'Pick something')
+        string(name: 'PORT_OPTION', defaultValue: '9090', description: 'insert desired local port')
     }
 
-  	stages {
-  		stage('Clean Workspace') {
-			steps {
-				script {
-					deleteDir()
-				}
-           	}
+    stages {
+          stage('Clean Workspace') {
+            steps {
+                script {
+                    deleteDir()
+                }
+               }
         }
 
         stage('Checkout') {
-	  	steps {
-                	script {
-		  		checkout([$class : 'GitSCM', ,
-			    	doGenerateSubmoduleConfigurations: false,
-			    	submoduleCfg : [],
-			    	branches : [[name: "${env.BUILD_BRANCH}"]],
-			    	userRemoteConfigs : [[credentialsId: "${GIT_CREDENTIAL}", url: "${SOURCE_GIT_URL}"]]])
-				}
-	  		}
-		}
-
-		stage('Build go'){
-	  	 	steps {
+              steps {
                 script {
-					sh 'docker-compose up --build'
-				}
-	  		}
-		}
-	}
-}	
+                      checkout([$class : 'GitSCM', ,
+                    doGenerateSubmoduleConfigurations: false,
+                    submoduleCfg : [],
+                    branches : [[name: "${env.BUILD_BRANCH}"]],
+                    userRemoteConfigs : [[credentialsId: "${GIT_CREDENTIAL}", url: "${SOURCE_GIT_URL}"]]])
+                }
+              }
+        }
+
+        stage('Build & deploy go'){
+                input{
+                    message "choose the file you want to build"
+                    parameters {
+                           choice(name: 'DOCKER_OPTION', choices: ['docker-compose', 'dockerfile'], description: 'Pick something')
+                    }
+                }
+                
+                steps {
+                    script{   
+                        if (env.DOCKER_OPTION == 'dockerfile'){
+                            sh "docker build ."
+                            //worked
+                        }else {
+                            sh "docker run -e MODE=${params.MODE} -p ${params.PORT_OPTION}:9090 -it {id or docker tag, como y de donde lo saco}"
+                        }
+                        
+                    }
+                    //"docker run -e MODE=${params.MODE} -p ${params.PORT_OPTION}:9090 -it {id or docker tag}"puerto
+                    //sh 'docker run -e MODE=cli -it(${params.OPTIONS}) {id or docker tag}'
+                }
+        }
+    }    
+    post { 
+        success { 
+            sh 'echo done ;)'//end program ctrl+c
+        }    
+        //failure {
+        //    mail to: soledad.kuczala@synclab.net, subject: 'The Pipeline failed :('
+        //}
+    }
+}
