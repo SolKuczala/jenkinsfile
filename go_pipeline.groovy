4b9f9d2e9a4cca27891d3ece253aa6b50f821826
e5471 <soledadnatalia.kuczala@igt.com>
2020-06-23 16:23:23 +0200

    remove steps for dockerfile

--- go_pipeline.groovy
+++ go_pipeline.groovy
@@ -5,11 +5,6 @@ pipeline {
         go 'GO'
       }
 
-    parameters {
-        choice(name: 'MODE', choices: ['api', 'cli'], description: 'Pick something')
-        string(name: 'PORT_OPTION', defaultValue: '9090', description: 'insert desired local port')
-    }
-
     stages {
           stage('Clean Workspace') {
             steps {
@@ -32,34 +27,15 @@ pipeline {
         }
 
         stage('Build & deploy go'){
-                input{
-                    message "choose the file you want to build"
-                    parameters {
-                           choice(name: 'DOCKER_OPTION', choices: ['docker-compose', 'dockerfile'], description: 'Pick something')
-                    }
-                }
-                
                 steps {
-                    script{   
-                        if (env.DOCKER_OPTION == 'dockerfile'){
-                            sh "docker build ."
-                            //worked
-                        }else {
-                            sh "docker run -e MODE=${params.MODE} -p ${params.PORT_OPTION}:9090 -it {id or docker tag, como y de donde lo saco}"
-                        }
-                        
+                        sh "docker-compose up --build"
                     }
-                    //"docker run -e MODE=${params.MODE} -p ${params.PORT_OPTION}:9090 -it {id or docker tag}"puerto
-                    //sh 'docker run -e MODE=cli -it(${params.OPTIONS}) {id or docker tag}'
                 }
         }
-    }    
+       
     post { 
         success { 
             sh 'echo done ;)'//end program ctrl+c
         }    
-        //failure {
-        //    mail to: soledad.kuczala@synclab.net, subject: 'The Pipeline failed :('
-        //}
     }
 }
